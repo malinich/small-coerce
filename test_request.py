a40cf70b9f5d2fc4bd7168aa84633338e4a3f23b
malinich <miltrix@yandex.ru>
2015-09-29 17:00:03 +0300

    добавлена опция для словарей

--- test_request.py
+++ test_request.py
@@ -1,6 +1,4 @@
-from validate import validate_request
-
-from unittest import TestCase
+from validate import coerce_to
 
 
 class Pa(object):
@@ -8,26 +6,29 @@ class Pa(object):
         self.x = x
         self.y = y
 
-    def __call__(self, *args, **kwargs):
-        print self.__class__
-        return self.x
-
     def __add__(self, other):
-        return self.x + other
+        return self.x + self.y + other
 
     def __str__(self):
         return '{}, {}'.format(self.x, self.y)
 
-@validate_request(('x', Pa), ('y', int), side_effect=TypeError)
+@coerce_to(('x', Pa), ('y', int), side_effect=TypeError)
 def v1(x, y):
-    print 'x: ', x
     return x + y
-
-b = 4
-
-# res = v1((3, 4), b)
-# assert 7 == res
 
 
 res = v1((3, 4), 34)
-assert 37 == res
+assert 41 == res
+
+
+
+@coerce_to(("arg1", {"some_key1": int, "some_key2": str},))
+def foo(arg1):
+    assert isinstance(arg1['some_key1'], int)
+    assert isinstance(arg1['some_key2'], str)
+    return arg1
+
+d = {"some_key1": '5', "some_key2": 10}
+result = foo(d)
+assert result['some_key1'] + 10 == 15
+assert result['some_key2'] + '10' == '1010'
--- validate.py
+++ validate.py
@@ -4,8 +4,20 @@ from __future__ import unicode_literals
 import collections
 import inspect
 
+def call_func(f, val, acc=None):
+    if isinstance(f, dict):
+        acc = {}
+        for ff, vv in f.items():
+            d = {ff: call_func(f[ff], val[ff], acc)}
+            acc.update(d)
 
-def validate_request(*fields, **kwargs):
+    else:
+        val = val if isinstance(val, tuple) else (val,)
+        return f(*val)
+    return acc
+
+
+def coerce_to(*fields, **kwargs):
     side_effect = kwargs.pop('side_effect', Exception)
 
     field_map = collections.OrderedDict()
@@ -13,12 +25,13 @@ def validate_request(*fields, **kwargs):
         field_map[field] = type_t
 
     def validate(kwargs):
-        f, val = kwargs[0], isinstance(kwargs[1], tuple) and kwargs[1] or (kwargs[1], )
+
+        f, val = kwargs[0], (kwargs[1], )
         try:
-            mapped = field_map[f](*val)
+            mapped = call_func(field_map[f], *val)
         except (ValueError, TypeError) as e:
             raise side_effect(e)
-        return (f, mapped)
+        return f, mapped
 
     def wrap(func):
         def wrap1(*args, **kwargs):
